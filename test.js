bb33d2bf397ed69094a1cdada4f1a6ec48cb5274
safwen-code <safwendjebbi1234@gmail.com>
2023-12-15 22:46:34 +0100

    first look of ejs

--- test.js
+++ test.js
@@ -1,23 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain')
-  res.send('Vs ete a page')
-})
-
-app.get('/sous', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain')
-  res.send('Vs ete a sous')
-})
-
-app.get('/etape/1', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain')
-  res.send('Vs ete a etatp')
-})
-app.use((req, res, next) => {
-  res.setHeader('Content-Type', 'text/plain')
-  res.status(404).send('page introuvable')
+app.get('/:id', (req, res) => {
+  res.render('chambre.ejs', { etape: req.params.id })
 })
 
 app.listen(5000)
