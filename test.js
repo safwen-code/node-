863ee271060e6b8a705dea921295229e0c25d1bd
safwen-code <safwendjebbi1234@gmail.com>
2023-12-15 22:52:49 +0100

    map number from params

--- test.js
+++ test.js
@@ -5,4 +5,8 @@ app.get('/:id', (req, res) => {
   res.render('chambre.ejs', { etape: req.params.id })
 })
 
+app.get('/nombre/:id', (req, res) => {
+  var noms = ['Robert', 'jacques', 'David']
+  res.render('pages.ejs', { comp: req.params.id, noms })
+})
 app.listen(5000)
