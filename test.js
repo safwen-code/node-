417edd3621d9bd6dcf8a2dc4e35758d533d71135
safwen-code <safwendjebbi1234@gmail.com>
2023-12-15 22:26:09 +0100

    Emit event

--- test.js
+++ test.js
@@ -1,17 +1,18 @@
 const http = require('http')
 const url = require('url')
 const querystring = require('querystring')
+const { emit } = require('process')
+const EventEmitter = require('events').EventEmitter
 
+const jeu = new EventEmitter()
+jeu.on('gameover', function (message) {
+  console.log(message)
+})
+jeu.emit('neauveu jeu', 'vs avez perdu', 35)
 const server = http.createServer((req, res) => {
   res.writeHead(200)
   res.write('donner non et prenom')
   res.end()
 })
 
-server.on('close', function () {
-  console.log('bye bye')
-})
-
 server.listen(5000)
-
-server.close()
