f097590c49d1d9b266d732876f7ccd5777defa3a
safwen-code <safwendjebbi1234@gmail.com>
2023-12-15 22:05:28 +0100

    test pages

--- test.js
+++ test.js
@@ -4,8 +4,14 @@ const url = require('url')
 const server = http.createServer((req, res) => {
   var page = url.parse(req.url).pathname
   console.log(page)
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  res.write('<p>Voici <strong>html</strong></p>')
+  //   res.writeHead(200, { 'Content-Type': 'text/html' })
+  if (page == '/') {
+    res.write('accueil')
+  } else if (page == '/sous') {
+    res.write('sous page')
+  } else if (page == '/etape/1') {
+    res.write('etap')
+  }
   res.end()
 })
 
